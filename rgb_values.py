8fc468a829ec7f0312943927dedade2cca979629
Ubuntu <ubuntu@ip-172-31-76-11.ec2.internal>
2020-12-19 17:38:10 +0000

    This is a colour identifier.

---
+++ rgb_values.py
@@ -0,0 +1,24 @@
+# Unit4-06/Usr/bin/env python3
+# THis program was created by larry NKengbeza
+# This program was created on December 2020
+# Thos [rpgram is a Colour identifiee.
+
+def main():
+    red = 25500
+    yellow = 2552550
+    orange = 2551650
+    # input
+    user_input = int(str(input("Enter a colour: ")))
+    print("")
+
+    # Process and Output
+    if user_input == red:
+        print("The colour is red")
+    elif user_input == yellow:
+        print("The colour is yellow")
+    elif user_input == orange:
+        print("The colour is orange")
+
+
+if __name__ == "__main__":
+    main()
